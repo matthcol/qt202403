74e06d2a5476d50da49285c3b43ea03f11a61bfe
matthcol <matthias.colin@gmail.com>
2024-03-20 12:57:55 +0100

    improve tree with 3 levels

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -21,16 +21,26 @@ std::unique_ptr<TreeItem> personTreeData(){
     PersonM* person1 = new PersonM("John Doe", QDate(1950,5, 6));
     PersonM* person21 = new PersonM("Jane Doe", QDate(1970,5, 6));
     PersonM* person22 = new PersonM("Emil Doe", QDate(1971,5, 6));
+    PersonM* person23 = new PersonM("Basil Doe", QDate(1972,5, 6));
+    PersonM* person31 = new PersonM("Bjorn Doe", QDate(1992,5, 6));
     std::unique_ptr<TreeItem> root = std::make_unique<TreeItem>(nullptr);
     TreeItem* parent = root.get();
     std::unique_ptr<TreeItem> firstPerson = std::make_unique<TreeItem>(
         person1, parent);
     parent = firstPerson.get();
-    firstPerson->appendChild(std::make_unique<TreeItem>(
-        person21, parent
+    std::unique_ptr<TreeItem> secondPerson = std::make_unique<TreeItem>(
+        person21, parent);
+    parent = secondPerson.get();
+    secondPerson->appendChild(std::make_unique<TreeItem>(
+        person31, parent
         ));
+    firstPerson->appendChild(std::move(secondPerson));
+    parent = firstPerson.get();
     firstPerson->appendChild(std::make_unique<TreeItem>(
         person22, parent
+        ));
+    firstPerson->appendChild(std::make_unique<TreeItem>(
+        person23, parent
         ));
     root->appendChild(std::move(firstPerson));
     return root;
